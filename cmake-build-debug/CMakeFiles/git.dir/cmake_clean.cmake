255125968e7e492a72ced3254dc2e94b9c750dce
Michal <236700@edu.p.lodz.pl>
2022-01-13 19:57:46 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/git.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/git.dir/main.c.obj"
+  "git.exe"
+  "git.exe.manifest"
+  "git.pdb"
+  "libgit.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/git.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
